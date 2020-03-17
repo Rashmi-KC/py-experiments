237fe8a606975ba14cdbf654a7596b9b43eb59eb
Rashmi K.C <rashmikc80@gmail.com>
2020-03-17 14:54:58 +0545

    Add 020odd-or-even

--- 01-character-input.py
+++ 01-character-input.py
@@ -1,4 +1,5 @@
 # Question 1
+# Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
 # https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
 
 name = input('Enter your name: ')
--- 02-odd-or-even.py
+++ 02-odd-or-even.py
@@ -1,2 +1,9 @@
 # Question 2
+# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
 # https: // www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+
+num = int(input('Enter a number: '))
+if num % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
