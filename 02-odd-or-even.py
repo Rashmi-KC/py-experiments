83c19f62f4830bf029669b06836c51fd65172f2e
Rashmi K.C <rashmikc80@gmail.com>
2020-03-17 17:53:05 +0545

    Add 02-odd-or-even

--- 02-odd-or-even.py
+++ 02-odd-or-even.py
@@ -1,5 +1,4 @@
 # Question 2
-# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
 # https: // www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
 
 num = int(input('Enter a number: '))
